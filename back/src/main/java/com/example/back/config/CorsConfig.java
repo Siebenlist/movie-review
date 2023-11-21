ef35e119f1ad4fc525ee4e571b19d8d0c5d7a858
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-11-20 22:03:07 -0300

    Update CorsConfig.java

--- back/src/main/java/com/example/back/config/CorsConfig.java
+++ back/src/main/java/com/example/back/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/auth/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }

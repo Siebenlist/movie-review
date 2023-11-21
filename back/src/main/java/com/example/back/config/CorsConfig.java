d9c254e2c395c8038cbbe533c5e61cde9e67dba2
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-11-21 18:55:27 -0300

    cambios en el cors

--- back/src/main/java/com/example/back/config/CorsConfig.java
+++ back/src/main/java/com/example/back/config/CorsConfig.java
@@ -1,15 +1,21 @@
 package com.example.back.config;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/auth/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer CORSConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT","DELETE");
+            }
+        };
     }
+
 }

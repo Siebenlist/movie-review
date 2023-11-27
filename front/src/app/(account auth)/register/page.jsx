b4b4136faccb7a00a8f1e7694f5b7d5bae433b97
Santiago Nicolas Rodriguez <48725347+Siebenlist@users.noreply.github.com>
2023-11-27 17:46:26 -0300

    Register changes

--- front/src/app/(account auth)/register/page.jsx
+++ front/src/app/(account auth)/register/page.jsx
@@ -1,28 +1,18 @@
 "use client";
 
 import { useState } from "react";
-<<<<<<< Updated upstream
-=======
 import { saveLocalStorageData } from "@/controllers/localStorageController";
 
 import { useRouter } from "next/navigation";
 
 import { userContext } from "@/context/propContext";
 import { useContext } from "react";
->>>>>>> Stashed changes
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-<<<<<<< Updated upstream
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("/auth/register", {
-=======
   const { setUserLogged } = useContext(userContext);
 
   const router = useRouter();
@@ -33,18 +23,10 @@ const Register = () => {
 
     try {
       const response = await fetch("http://localhost:8080/auth/register", {
->>>>>>> Stashed changes
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-<<<<<<< Updated upstream
-        body: JSON.stringify({ username, email, password }),
-      });
-
-      if (response.ok) {
-        console.log("Registracion exitosa");
-=======
         body: JSON.stringify(submitData),
       });
 
@@ -64,7 +46,6 @@ const Register = () => {
 
         router.push("/");
         router.refresh();
->>>>>>> Stashed changes
       } else {
         console.error("Registracion fallada");
       }
@@ -79,10 +60,6 @@ const Register = () => {
         <h1 className="text-4xl font-bold">Register</h1>
         <form
           className="flex flex-col gap-10  text-black"
-<<<<<<< Updated upstream
-          action=""
-=======
->>>>>>> Stashed changes
           onSubmit={handleSubmit}
         >
           <input

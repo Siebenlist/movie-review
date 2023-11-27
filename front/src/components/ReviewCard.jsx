73f72859de8e34f3612373997edee31cfb8dad7e
Santiago Nicolas Rodriguez <48725347+Siebenlist@users.noreply.github.com>
2023-11-27 16:49:21 -0300

    Revert "Profile code"
    This reverts commit 180dc2f3ed5c01d1a89b401b57656ea320f473ea.

--- front/src/components/ReviewCard.jsx
+++ front/src/components/ReviewCard.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import reviewStar from "../assets/reviewStar.svg";
-import Image from "next/image";
 
 const ReviewCard = ({ id, poster, rating, movie, user, review, pfp }) => {
   return (
-    <article className="review flex gap-10 p-3" key={id}>
-      <div className="flex flex-col items-center max-w-[200px]">
-        <img className="w-full" src={poster} />
-        <p className="inline-flex items-center gap-2 my-2">
-          Rating: {rating}{" "}
-          <Image src={reviewStar} alt="review star" width={20} />
-        </p>
+    <article
+      className="review flex justify-center gap-5 mx-auto w-[400px] p-3"
+      key={id}
+    >
+      <div className="flex flex-col items-center">
+        <img className="w-[100px]" src={poster} />
+        <p>Rating: {rating}</p>
       </div>
-      <div className="flex flex-col justify-start gap-2">
+      <div className="flex flex-col justify-start gap-2 max-w-[400px]">
         <h3>{movie}</h3>
         <div className="flex items-center gap-3">
           <img className="w-[30px]" src={pfp} alt={user} />
-          <a href="#" className="font-semibold">
-            {user}
-          </a>
+          <a href="#">{user}</a>
         </div>
-        <p className="w-full">{review}</p>
+        <p>{review}</p>
       </div>
     </article>
   );
--- front/src/context/propContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const userContext = createContext(); //Crea el contexto del usuario para manejar la sesion

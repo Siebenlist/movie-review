e5f3fc7b484f87a56ed7b66bce54d4daaa2a735b
Santiago Nicolas Rodriguez <48725347+Siebenlist@users.noreply.github.com>
2023-12-05 19:28:38 -0300

    Fetcheo al back para favoritos

--- front/src/components/MovieActions.jsx
+++ front/src/components/MovieActions.jsx
@@ -1,9 +1,36 @@
+"use client";
+
+import { useState } from "react";
 import React from "react";
 import watchlistIcon from "@/assets/watchlistIcon.svg";
 import favMovieIcon from "@/assets/favMovieIcon.svg";
 import RatingComponent from "./RatingComponent";
+import { getStorageData } from "@/controllers/localStorageController";
 
-const MovieActions = () => {
+const MovieActions = ({ movieId }) => {
+  const [movieFaved, setMovieFaved] = useState(null);
+
+  const userData = getStorageData();
+  const username = userData.user;
+
+  const handleFav = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/fav", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ movieId, username }),
+      });
+
+      if (res.ok) {
+        console.log("Uusario", username);
+      }
+    } catch {
+      alert("Erro ao adicionar a lista de favoritos");
+    }
+  };
+
   return (
     <div className="bg-menu rounded-md p-2 mb-2">
       <div className="flex justify-center gap-3">
@@ -11,7 +38,11 @@ const MovieActions = () => {
           <img src={watchlistIcon.src} alt="Watchlist eye icon" />
         </button>
         <button className="w-[35px]">
-          <img src={favMovieIcon.src} alt="Watchlist eye icon" />
+          <img
+            onClick={handleFav}
+            src={favMovieIcon.src}
+            alt="Watchlist eye icon"
+          />
         </button>
       </div>
       <div className="text-center">

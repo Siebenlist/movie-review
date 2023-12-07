5a6a79071b7161ac675dcaa4a8946fc41a3cc6b2
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-12-07 17:17:32 -0300

    rating 2.0

---
+++ back/src/main/java/com/example/back/rating/Rating.java
@@ -0,0 +1,31 @@
+package com.example.back.rating;
+
+import com.example.back.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="rating")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer movieId;
+    private Integer rating;
+    @Column(updatable = false)
+    private Date created_at;
+    private Date updated_at;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+}
---
+++ back/src/main/java/com/example/back/rating/RatingRequest.java
@@ -0,0 +1,16 @@
+package com.example.back.rating;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RatingRequest {
+    private Integer movieId;
+    private Integer rating;
+    private String username;
+}

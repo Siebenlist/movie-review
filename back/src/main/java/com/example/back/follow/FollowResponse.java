93bd341339c8893a756a0a6ce4d986e59babc5af
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-12-09 20:06:14 -0300

    follow actualizado con request
    falta metodo get

---
+++ back/src/main/java/com/example/back/follow/FollowResponse.java
@@ -0,0 +1,14 @@
+package com.example.back.follow;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowResponse {
+    private Integer id;
+}

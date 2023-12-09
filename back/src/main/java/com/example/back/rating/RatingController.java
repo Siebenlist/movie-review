7c0d63cd0694f63537d3e38d314ba02d4b96a908
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-12-09 20:19:06 -0300

    comiteo para pullear

--- back/src/main/java/com/example/back/rating/RatingController.java
+++ back/src/main/java/com/example/back/rating/RatingController.java
@@ -1,8 +1,10 @@
 package com.example.back.rating;
 
+import com.example.back.favourite.FavouriteResponse;
 import com.example.back.user.User;
 import com.example.back.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ public class RatingController {
     private final UserRepository userRepository;
 
     @PostMapping(value = "/rating")
-    public Rating handleRating(@RequestBody RatingRequest ratingRequest) {
+    public ResponseEntity<RatingResponse> handleRating(@RequestBody RatingRequest ratingRequest) {
         User user = userRepository.findUserByUsername(ratingRequest.getUsername());
         if(user == null) {
             return null;
@@ -28,11 +30,15 @@ public class RatingController {
                     .user(user)
                     .build();
             ratingRepository.save(rating);
-            return rating;
+            return ResponseEntity.ok(RatingResponse.builder()
+                    .id(rating.getId())
+                    .build());
         } else {
             actualRating.setRating(ratingRequest.getRating());
             ratingRepository.save(actualRating);
-            return actualRating;
+            return ResponseEntity.ok(RatingResponse.builder()
+                    .id(actualRating.getId())
+                    .build());
         }
     }
 

40f47355c37d22eee66d586b056c1d168ad966dc
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-12-10 02:22:03 -0300

    feat(ratings): Agregando el rating global a cada película

--- back/src/main/java/com/example/back/rating/RatingRepository.java
+++ back/src/main/java/com/example/back/rating/RatingRepository.java
@@ -3,9 +3,12 @@ package com.example.back.rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
     Integer countByMovieId(Integer movieId);
     Rating findByMovieIdAndUserId(Integer movieId, Integer userId);
-    
+
+    List<Rating> findAllByMovieId(Integer movieId); //Para sacar el promedio de las calificaciones de una pelicula
 }

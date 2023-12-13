3e8701a71fe421d5b6502813b6e65776d0dbd904
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-12-12 22:26:15 -0300

    watchlist terminado

---
+++ back/src/main/java/com/example/back/watchlist/WatchlistController.java
@@ -0,0 +1,70 @@
+package com.example.back.watchlist;
+
+import com.example.back.favourite.Favourite;
+import com.example.back.favourite.FavouriteResponse;
+import com.example.back.user.User;
+import com.example.back.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class WatchlistController {
+    private final WatchlistRepository watchlistRepository;
+    private final UserRepository userRepository;
+    @PostMapping(value = "/handleWatchlist")
+    public ResponseEntity<WatchlistResponse> handleWatchlist(@RequestBody WatchlistRequest watchlistRequest) {
+        String username = watchlistRequest.getUsername();
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        Watchlist watch = watchlistRepository.findByMovieIdAndUserId(watchlistRequest.getMovieId(), user.getId());
+        if (watch == null) {
+            Watchlist newWatch = Watchlist.builder()
+                    .movieId(watchlistRequest.getMovieId())
+                    .user(user)
+                    .build();
+            watchlistRepository.save(newWatch);
+            return ResponseEntity.ok(WatchlistResponse.builder()
+                    .id(newWatch.getId())
+                    .build());
+        }
+        watchlistRepository.delete(watch);
+        return ResponseEntity.ok(WatchlistResponse.builder()
+                .id(null)
+                .build());
+
+    }
+
+    @GetMapping(value = "/watchlisted")
+    public ResponseEntity<WatchlistResponse> getSingularWatchlist(@RequestParam String username, @RequestParam Integer movieId) {
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        Watchlist watch = watchlistRepository.findByMovieIdAndUserId(movieId, user.getId());
+        if (watch == null) {
+            return ResponseEntity.ok(WatchlistResponse.builder()
+                    .id(null)
+                    .build());
+        }
+        return ResponseEntity.ok(WatchlistResponse.builder()
+                .id(watch.getId())
+                .build());
+    }
+
+    @GetMapping(value = "/watchlist")
+    public ResponseEntity<WatchlistUserResponse> getWatchlist(@RequestParam String username) {
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(WatchlistUserResponse.builder()
+                .watchlist(watchlistRepository.findAllByUserId(user.getId()))
+                .build());
+    }
+
+}

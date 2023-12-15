56e9d3c7506ce6310f156ed57de52ac4e4f90075
Santiago Nicolas Rodriguez <siebenlist.sr@gmail.com>
2023-12-14 23:22:13 -0300

    watchlist data finished

--- front/src/app/profile/[username]/watchlist/page.jsx
+++ front/src/app/profile/[username]/watchlist/page.jsx
@@ -1,7 +1,78 @@
-import MoviePoster from "@/components/MoviePoster";
-import { movies } from "@/data/movies";
+"use client";
 
-const Watchlist = () => {
+import MoviePoster from "@/components/MoviePoster";
+import { useEffect, useState } from "react";
+import { getStorageData } from "@/controllers/localStorageController";
+
+const Watchlist = ({ params }) => {
+  const userData = JSON.parse(getStorageData());
+  const [watchlist, setWatchlist] = useState([]);
+  const [watchlistPosters, setWatchlistPosters] = useState([]);
+
+  const fetchWatchlistPosters = async () => {
+    const paths = await Promise.all(
+      watchlist.map(async (watch) => movieDataFetch(watch.movieId))
+    );
+    setWatchlistPosters(paths);
+  };
+
+  const movieDataFetch = async (id) => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNmNhYTNjZDBlZmRjODI2ZWRhNWVkNWYyMWZlMDllMiIsInN1YiI6IjYzNmY4YjBiZDdmYmRhMDA5MDVkOTJjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0IbstPtIVklqlKXghzWLmq2AGigTFlb2cCWbPEZhf0M",
+      },
+    };
+
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+        options
+      );
+      const data = await response.json();
+      return {
+        path: data.poster_path,
+        id: data.id,
+      };
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getWatchlist = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${userData.token}`,
+      },
+    };
+
+    try {
+      const res = await fetch(
+        `http://localhost:8080/watchlist?username=${params.username}`,
+        options
+      );
+      if (res.ok) {
+        const data = await res.json();
+        setWatchlist(data.watchlist);
+        console.log("watchlist data", data.watchlist);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getWatchlist();
+  }, []);
+
+  useEffect(() => {
+    fetchWatchlistPosters();
+  }, [watchlist]);
+
   return (
     <section className="max-w-[1200px] h-full mx-auto">
       <div className="flex flex-col w-full md:w-[1200px]">
@@ -9,13 +80,13 @@ const Watchlist = () => {
           <p className="text-slate uppercase">Fav films</p>
           <div className="max-w-full h-[1px] bg-gray"></div>
         </div>
-        <div className="grid grid-cols-3 md:grid-cols-6 auto-rows-max mt-2 place-items-center gap-2 w-full h-[500px] overflow-y-scroll">
-          {movies.map((movie) => {
+        <div className="grid grid-cols-3 md:grid-cols-6 auto-rows-max mt-2 place-items-center gap-2 p-5 w-full h-[500px] overflow-y-scroll">
+          {watchlistPosters.map((watchlistItem, index) => {
             return (
               <MoviePoster
-                id={movie.id}
-                poster={movie.poster}
-                title={movie.title}
+                key={watchlistItem.id} // Corregir la clave a watchlist[index].id
+                id={watchlist[index].movieId}
+                poster={watchlistPosters[index].path}
               />
             );
           })}

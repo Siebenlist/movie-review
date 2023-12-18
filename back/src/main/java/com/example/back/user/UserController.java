87a3c665e9492354c4ab6c734a296d147609b486
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-12-18 15:40:11 -0300

    Update UserController.java

--- back/src/main/java/com/example/back/user/UserController.java
+++ back/src/main/java/com/example/back/user/UserController.java
@@ -23,23 +23,26 @@ public class UserController {
 
 
     @PostMapping(value = "/userInfo")
-    public void updateUser(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<?> updateUser(@RequestBody UserRequest userRequest) {
         User user = userRepository.findUserByUsername(userRequest.getActualUsername());
         if(userRequest.getNewUsername() != null) {
             if(userRepository.findUserByUsername(userRequest.getActualUsername()) != null) {
                 user.setUsername(userRequest.getNewUsername());
                 userRepository.save(user);
+                return ResponseEntity.ok("Username updated successfully");
             }
         }
         if(userRequest.getEmail() != null) {
             if(userRepository.findUserByEmail(userRequest.getEmail()) != null) {
                 user.setEmail(userRequest.getEmail());
                 userRepository.save(user);
+                return ResponseEntity.ok("Email updated successfully");
             }
         }
         if(userRequest.getPassword() != null) {
             user.setPassword(userRequest.getPassword());
             userRepository.save(user);
+            return ResponseEntity.ok("Password updated successfully");
         }
     }
 }

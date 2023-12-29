a9150d2d57c136dd78c00606f7bd0095bc595310
Santiago Nicolas Rodriguez <siebenlist.sr@gmail.com>
2023-12-29 20:50:52 -0300

    styles: changed width

--- front/src/components/ReviewCard.jsx
+++ front/src/components/ReviewCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const ReviewCard = ({ id, poster, rating, movie, user, review, pfp, date }) => {
   return (
-    <article className="review flex gap-10 p-5" key={id}>
+    <article className="review flex gap-10 p-5 w-full" key={id}>
       <div className="hidden md:flex flex-col items-center w-[100px]">
         <img
           className="w-full"
@@ -24,7 +24,7 @@ const ReviewCard = ({ id, poster, rating, movie, user, review, pfp, date }) => {
           </a>
         </div>
         <p className="break-words">{review}</p>
-          <p className="text-sm text-gray">Reviewed at: {date}</p>
+        <p className="text-sm text-gray">Reviewed at: {date}</p>
       </div>
     </article>
   );
